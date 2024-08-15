5e46d661a943eddec365ad1f15e2041632bced81
clarencechok <113416035+clarencechok@users.noreply.github.com>
2024-08-15 17:42:14 +0800

    Final mechanics addition + volume and sound adjustments

--- Assets/Scripts/UI/SelectionArrow.cs
+++ Assets/Scripts/UI/SelectionArrow.cs
@@ -1,71 +1,60 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+
 public class SelectionArrow : MonoBehaviour
 {
-    //To get the y positions of all the options
-    [SerializeField] private RectTransform[] options;
-    private RectTransform rect;
-    //this variable will tell us currently which option is selected
-    private int currentSelectedPosition;
-    //this variable is to play the sound when we change options or move the arrow up and down
-    [SerializeField] private AudioClip ChangeOptionSound;
-    //this variable is to play the sound when 1 option is selected and we press enter
-    [SerializeField] private AudioClip interactionSound;
+    [SerializeField] private RectTransform[] buttons;
+    [SerializeField] private AudioClip changeSound;
+    [SerializeField] private AudioClip interactSound;
+    private RectTransform arrow;
+    private int currentPosition;
 
     private void Awake()
     {
-        //to gain access to rectTransform
-        rect = GetComponent<RectTransform>();
+        arrow = GetComponent<RectTransform>();
     }
-
+    private void OnEnable()
+    {
+        currentPosition = 0;
+        ChangePosition(0);
+    }
     private void Update()
     {
-        //check if up arrow key or W key is pressed
+        //Change the position of the selection arrow
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-        {
-            //move arrow up if either key is pressed down
-            ChangingPosition(-1);
-        }else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-        {
-            ChangingPosition(1);
-        }
+            ChangePosition(-1);
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            ChangePosition(1);
 
-        //interactions with options
+        //Interact with current option
         if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.E))
-        {
             Interact();
-        }
     }
 
-    private void ChangingPosition(int _change)
+    private void ChangePosition(int _change)
     {
-        currentSelectedPosition += _change;
+        currentPosition += _change;
 
-        //to check if there is change in option
         if (_change != 0)
-        {
-            SoundManager.instance.PlaySound(ChangeOptionSound);
-        }
+            SoundManager.instance.PlaySound(changeSound);
 
-        //to solve the issue of current position of arrow being negative or larger than length of array
-        if (currentSelectedPosition < 0)
-        {
-            currentSelectedPosition = options.Length - 1;
-        }else if (currentSelectedPosition > options.Length - 1)
-        {
-            currentSelectedPosition = 0;
-        }
+        if (currentPosition < 0)
+            currentPosition = buttons.Length - 1;
+        else if (currentPosition > buttons.Length - 1)
+            currentPosition = 0;
 
-        //moving up and down of the arrow
-        rect.position = new Vector3(rect.position.x, options[currentSelectedPosition].position.y, 0);
+        AssignPosition();
     }
-
+    private void AssignPosition()
+    {
+        //Assign the Y position of the current option to the arrow (basically moving it up and down)
+        arrow.position = new Vector3(arrow.position.x, buttons[currentPosition].position.y);
+    }
     private void Interact()
     {
-        //play the interaction sound
-        SoundManager.instance.PlaySound(interactionSound);
+        SoundManager.instance.PlaySound(interactSound);
 
-        //access button component on every option and call own function
-        options[currentSelectedPosition].GetComponent<Button>().onClick.Invoke();
+        //Access the button component on each option and call its function
+        buttons[currentPosition].GetComponent<Button>().onClick.Invoke();
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,17 +5,34 @@ public class UIManager : MonoBehaviour
 {
     //adding a reference to the ghame over screen to activate or deactivate
     [SerializeField] private GameObject gameOverScreen;
-
+    [Header ("Game Over")]
     //this music will be played when the game over menu appears
     [SerializeField] private AudioClip gameOverSound;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
 
     private void Awake()
     {
         //deactivate game over screen
         gameOverScreen.SetActive(false);
+        //deactivate pause screen
+        pauseScreen.SetActive(false);
     }
 
-    //this will be responsible for handling the gameover screen and playing game over sound
+    private void Update()
+    {
+        //check if the escape key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //account for scenario where the game is already paused
+            PauseGame(!pauseScreen.activeInHierarchy);
+
+        }
+    }
+
+    #region Game Over
+    // be responsible for handling the gameover screen and playing game over sound
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
@@ -37,7 +54,40 @@ public class UIManager : MonoBehaviour
     //Quit game/exit play mode if in Editor
     public void Quit()
     {
-        Application.Quit(); //Quits the game (only works in build)
-        UnityEditor.EditorApplication.isPlaying = false; //Exits play mode
+        //Quits the game but this code only work in build
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        //Exits play mode but only work in the editor
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
+    #endregion
+
+
+    #region Pause
+
+    public void PauseGame(bool status)
+    {
+        //if true will pause game but if false will unpause
+        pauseScreen.SetActive(status);
+
+        //When pause status is true change timescale to 0 (time stops)
+        //when it's false change it back to 1 (time goes by normally)
+        if (status)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void SoundVolume()
+    {
+        SoundManager.instance.ChangeSoundVolume(0.2f);
+    }
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(0.2f);
+    }
+
+    #endregion
 }
